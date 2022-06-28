2f9565ee518b91a605bf684a957a66deda3cc104
James Sullivan <jsullivan2847@gmail.com>
2022-06-28 15:21:55 -0400

    first commit

---
+++ deliv.py
@@ -0,0 +1,40 @@
+
+# color = ''
+# while color != 'quit':
+#     color = input('Enter "green", "yellow", "red": ').lower()
+#     print(f'The user entered {color}')
+#     if color == 'green':
+#         print('Go!')
+#     elif color == 'yellow':
+#         print('Slow Down!')
+#     elif color == 'red':
+#         print('Stop!')
+#     elif color == "quit":
+#         print('ok you''re done')
+
+
+
+nums = list(range(10))
+print(nums)
+odds = tuple(range(1, 10, 2))
+print(odds)
+
+
+
+
+#in python an array is called a list 
+
+# names = ["Tom", "Deborah", "Murray", "Axel"]
+
+# age = 53
+
+# while age < 60:
+#     age += 1
+#     print(age)
+
+# # name refers to each individual name in the list 
+# for name in names:
+#   print(name)
+
+
+
